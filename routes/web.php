b7c0ad2a1f7281bc4a25c0ef4389e94c06196f4d
Lukas Yelle <lxy5611@g.rit.edu>
2019-03-18 20:36:51 +0000

    Apply fixes from StyleCI

--- routes/web.php
+++ routes/web.php
@@ -27,23 +27,17 @@ Route::get('logout', 'SAMLController@logout');
 /* Admin Pages */
 
 Route::prefix('admin')->group(function () {
-
     Route::get('/', 'Pages\AdminController@index')->name('dashboard');
 
     Route::prefix('spots')->group(function () {
-
         Route::get('types', 'Pages\AdminController@manageTypes')->name('types');
         Route::get('classification', 'Pages\AdminController@manageTypes')->name('classification');
         Route::get('categories', 'Pages\AdminController@manageTypes')->name('categories');
         Route::get('descriptors', 'Pages\AdminController@manageTypes')->name('descriptors');
-
     });
 
     Route::prefix('users')->group(function () {
-
         Route::get('all', 'Pages\AdminController@manageTypes')->name('all');
         Route::get('staff', 'Pages\AdminController@manageTypes')->name('staff');
-
     });
-
 });
