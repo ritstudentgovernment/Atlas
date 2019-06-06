a7ae4d350a8ce889aa99d3586a87c1bbd966b277
Lukas Yelle <lxy5611@g.rit.edu>
2019-06-06 18:53:31 -0400

    Tests new SAML configuration

--- config/saml2_settings.php
+++ config/saml2_settings.php
@@ -55,7 +55,7 @@ return $settings = [
     // or unencrypted messages if it expects them signed or encrypted
     // Also will reject the messages if not strictly follow the SAML
     // standard: Destination, NameId, Conditions ... are validated too.
-    'strict' => true,
+    'strict' => false,
 
     // Enable debug mode (to print errors)
     'debug' => env('APP_DEBUG', false),
