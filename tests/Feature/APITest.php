2994353ab840c32a732b54491675ab4e3039ae5f
Lukas Yelle <lxy5611@g.rit.edu>
2020-01-14 17:28:44 -0500

    Reverts the revert that reverted too much

--- tests/Feature/APITest.php
+++ tests/Feature/APITest.php
@@ -19,7 +19,7 @@ class APITest extends TestCase
      *
      * @return void
      */
-    public function setUp()
+    public function setUp(): void
     {
         parent::setUp();
         $this->spot = (factory(Spot::class))->create(['approved' => false]);
