787bbee07d879f8762610f76fb46e865e2ac96de
Lukas Yelle <lxy5611@g.rit.edu>
2020-01-21 14:30:57 -0500

    Updates the SG Top Bar to reflect new changes in the organization.

--- resources/views/includes/sg_nav.blade.php
+++ resources/views/includes/sg_nav.blade.php
@@ -1,4 +1,4 @@
-<style>.dropbtn,.dropbtn-mobile{background-color:#f70;color:#fff;font-size:12px;border:none;outline:0}.dropbtn,.dropbtn-mobile,.dropdown-left a{font-weight:700;cursor:pointer}#sg-global-nav{overflow:hidden;height:30px;padding:0}.dropbtn{position:relative;padding-right:20px;padding-bottom:5px;padding-top:5px}.dropbtn-mobile{padding-right:1em;height:30px}.dropdown-left{text-align:left;position:fixed;display:inline-block;width:50%;z-index:99;height:30px;padding-left:5%;font-family:Montserrat;line-height:27px}.dropdown-right-main,.dropdown-right-mobile{z-index:99;height:30px;font-family:Montserrat;text-align:right}.dropdown-right-mobile{display:none;position:fixed;width:50%;margin-left:50%}.dropdown-content,.dropdown-content-mobile{position:absolute;min-width:160px;box-shadow:0 8px 16px 0 rgba(0,0,0,.2);z-index:1;text-align:left;font-size:12px}.dropdown-content{display:none;right:0;background-color:#f9f9f9;border-top:8px;border-top-style:solid;border-top-color:#f70;margin-top:5px;font-family:Montserrat}.dropdown-content a,.dropdown-content-mobile a,.dropdown-content-mobile p{display:block;font-family:Montserrat;text-decoration:none}.dropdown-content a{color:#000;padding:12px 16px}.dropdown-content-mobile p{color:#000;padding:8px 12px;margin-bottom:0;background-color:#d3d3d3}.dropdown-content a:hover{color:grey}.dropdown-content-mobile a{color:#000;padding:0 8px}.dropdown-content-mobile a:hover{color:grey}.dropdown-content-mobile{display:none;background-color:#f9f9f9;border-top-style:solid;border-top-color:#f70;font-family:Montserrat;right:10px;padding-bottom:10px}.dropdown-left a{background-color:#f70;color:#fff;font-size:12px;border:none;font-family:Montserrat;height:20px;text-align:center;margin-top:20px}.dropdown-left a:hover{text-decoration:none}.header-top{height:30px;background-color:#f70;z-index:1001;position:fixed;top:0;left:0;right:0}body,html{position:relative}.show{display:block}@media screen and (max-width:992px){.dropdown-right-main{display:none}.dropdown-right-mobile{display:inline-block}}</style>
+<style>.dropbtn,.dropbtn-mobile{background-color:#f70;color:#fff;font-size:12px;border:none;outline:0}.dropbtn,.dropbtn-mobile,.dropdown-left a{font-weight:700;cursor:pointer}#sg-global-nav{overflow:hidden;height:30px;padding:0}.dropbtn{position:relative;padding-right:20px;padding-bottom:5px;padding-top:5px}.dropbtn-mobile{padding-right:1em;height:30px}.dropdown-left{text-align:left;position:fixed;display:inline-block;width:50%;z-index:99;height:30px;padding-left:5%;font-family:Montserrat;line-height:27px}.dropdown-right-main,.dropdown-right-mobile{z-index:99;height:30px;font-family:Montserrat;text-align:right}.dropdown-right-mobile{display:none;position:fixed;width:50%;margin-left:50%}.dropdown-content,.dropdown-content-mobile{position:absolute;min-width:160px;box-shadow:0 8px 16px 0 rgba(0,0,0,.2);z-index:1;text-align:left;font-size:12px}.dropdown-content{display:none;right:0;background-color:#f9f9f9;border-top:8px;border-top-style:solid;border-top-color:#f70;margin-top:5px;font-family:Montserrat}.dropdown-content a,.dropdown-content-mobile a,.dropdown-content-mobile p{display:block;font-family:Montserrat;text-decoration:none}.dropdown-content a{color:#000;padding:12px 16px}.dropdown-content-mobile p{color:#000;padding:8px 12px;margin-bottom:0;background-color:#d3d3d3}.dropdown-content a:hover{color:grey}.dropdown-content-mobile a{color:#000;padding:0 8px}.dropdown-content-mobile a:hover{color:grey}.dropdown-content-mobile{display:none;background-color:#f9f9f9;border-top-style:solid;border-top-color:#f70;font-family:Montserrat;right:10px;padding-bottom:10px}.dropdown-left a{background-color:#f70;color:#fff;font-size:12px;border:none;font-family:Montserrat;height:20px;text-align:center;margin-top:20px}.dropdown-left a:hover{text-decoration:none}.header-top{height:30px;background-color:#f70;z-index:1001;position:fixed;top:0;left:0;right:0}body,html{position:relative}.show{display:block}@media screen and (max-width:992px){.dropdown-right-main{display:none}.dropdown-right-mobile{display:inline-block}}.white-text{color: #fff;}.white-text:hover{color: #fff; text-decoration: none;}</style>
 
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
@@ -17,8 +17,7 @@
                     <a href="https://sg.rit.edu/about/">Structure</a>
                     <a href="https://sg.rit.edu/about/mission-and-vision/">Mission and Vision</a>
                     <a href="https://sg.rit.edu/about/committees/">Committees</a>
-                    <a href="https://sg.rit.edu/about/msos/">MSO's</a>
-                    <a href="https://sg.rit.edu/awards/">Awards</a>
+                    <a href="https://sg.rit.edu/about/rsos/">RSO's</a>
                 </div>
             </button>
             <button id="2" onmouseover="openDropdown(this.id)" onmouseout="closeDropdown(this.id)" class="dropbtn">
@@ -27,32 +26,18 @@
                 <div id="myDropdown2" class="dropdown-content">
                     <a href="https://sg.rit.edu/services/">Services Overview</a>
                     <a href="https://pawprints.rit.edu/">Paw Prints</a>
-                    <a href="https://bikeshare.rit.edu/">Bike Share</a>
                     <a href="https://sg.rit.edu/legalaid/">Legal Aid</a>
                     <a href="https://ritpedia.rit.edu/Main_Page">RITpedia</a>
                     <a href="https://openevals.rit.edu/">Open Evals</a>
-                    <a href="https://sites.sg.rit.edu/">SG Sites</a>
                     <a href="https://sg.rit.edu/finance/">Finance</a>
-                    <a href="https://sg.rit.edu/breakbus/">Bike Share</a>
+                    <a href="https://sg.rit.edu/breakbus/">Break Bus</a>
                 </div>
             </button>
-            <button id="3" onmouseover="openDropdown(this.id)" onmouseout="closeDropdown(this.id)" class="dropbtn">
-                Resources
-                <i class="material-icons" style="font-size:12px;font-weight: bold;">keyboard_arrow_down</i>
-                <div id="myDropdown3" class="dropdown-content">
-                    <a href="https://sg.rit.edu/sgupdates/">SG Updates</a>
-                    <a href="https://sg.rit.edu/resources/">Resourse List</a>
-                    <a href="https://sg.rit.edu/resources/sg-logos/">SG Logos</a>
-                </div>
+            <button id="3" class="dropbtn">
+                <a href="https://sg.rit.edu/resources/" class="white-text">Resources</a>
             </button>
-            <button id="4" onmouseover="openDropdown(this.id)" onmouseout="closeDropdown(this.id)" class="dropbtn">Get
-                Involved
-                <i class="material-icons" style="font-size:12px;font-weight: bold;">keyboard_arrow_down</i>
-                <div id="myDropdown4" class="dropdown-content">
-                    <a href="https://sg.rit.edu/spirit/">Project SpiRIT</a>
-                    <a href="https://sg.rit.edu/get-involved/events-calendar/">Event Calendar</a>
-                    <a href="https://sg.rit.edu/get-involved/">Opportunities</a>
-                </div>
+            <button id="4" class="dropbtn">
+                <a href="https://sg.rit.edu/get-involved/" class="white-text">Get Involved</a>
             </button>
         </div>
         <div class="dropdown-right-mobile">
@@ -66,28 +51,22 @@
                     <a href="https://sg.rit.edu/about/">Structure</a>
                     <a href="https://sg.rit.edu/about/mission-and-vision/">Mission and Vision</a>
                     <a href="https://sg.rit.edu/about/committees/">Committees</a>
-                    <a href="https://sg.rit.edu/about/msos/">MSO's</a>
-                    <a href="https://sg.rit.edu/awards/">Awards</a>
+                    <a href="https://sg.rit.edu/about/rsos/">RSO's</a>
 
                     <p>Services</p>
                     <a href="https://sg.rit.edu/services/">Overview</a>
                     <a href="https://pawprints.rit.edu/">Paw Prints</a>
-                    <a href="https://bikeshare.rit.edu/">Bike Share</a>
                     <a href="https://sg.rit.edu/legalaid/">Legal Aid</a>
                     <a href="https://ritpedia.rit.edu/Main_Page">RITpedia</a>
                     <a href="https://openevals.rit.edu/">Open Evals</a>
                     <a href="https://sites.sg.rit.edu/">SG Sites</a>
                     <a href="https://sg.rit.edu/finance/">Finance</a>
-                    <a href="https://sg.rit.edu/breakbus/">Bike Share</a>
+                    <a href="https://sg.rit.edu/breakbus/">Break Bus</a>
 
                     <p>Resources</p>
-                    <a href="https://sg.rit.edu/sgupdates/">SG Updates</a>
-                    <a href="https://sg.rit.edu/resources/">Resourse List</a>
-                    <a href="https://sg.rit.edu/resources/sg-logos/">SG Logos</a>
+                    <a href="https://sg.rit.edu/resources/">Resources List</a>
 
                     <p>Get Involved</p>
-                    <a href="https://sg.rit.edu/spirit/">Project SpiRIT</a>
-                    <a href="https://sg.rit.edu/get-involved/events-calendar/">Event Calendar</a>
                     <a href="https://sg.rit.edu/get-involved/">Opportunities</a>
 
                 </div>
