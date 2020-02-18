535d269e703af6a8598a8851c22a458970577d6c
Lukas Yelle <lxy5611@g.rit.edu>
2020-02-18 13:22:45 -0500

    Verbage fixes

--- resources/views/emails/spots/created.blade.php
+++ resources/views/emails/spots/created.blade.php
@@ -1,7 +1,7 @@
 @component('mail::message')
 # Success!
 @if($spot->approved_classification_id)
-Your <b>{{ $spot->name }} Spot</b> has been successfully submitted for review, but it isn't published yet. You'll get an email when it is approved by SG's moderators!
+Your <b>{{ $spot->name }} Spot</b> has been successfully submitted for review, but it isn't published yet. You'll get an email when it is approved by {{ env('ORG_SHORT_NAME') }}'s moderators!
 @else
 Your <b>{{ $spot->name }} Spot</b> has been successfully created.
 @endif
--- resources/views/emails/spots/rejected.blade.php
+++ resources/views/emails/spots/rejected.blade.php
@@ -2,11 +2,13 @@
 # Thank you for using {{config('app.name')}}
 
 @if($spot->approved_classification)
-Your <b>{{ $spot->name }} Spot</b> was determined by {{ env('ORG_SHORT_NAME') }} moderators to not be eligible for publishing. Please feel free to try again, ensuring you are abiding by our community guidelines.
+Your <b>{{ $spot->name }} Spot</b> was determined by {{ env('ORG_SHORT_NAME') }}'s moderators to not be eligible for publishing. Please feel free to try again, ensuring you are abiding by our community guidelines.
 @else
-Your <b class="bold">{{ $spot->name }} Spot</b> has been removed by a moderator.<br />
-If you believe this was an error, please contact us at <a href="mailto:{{ env('CONTACT_EMAIL') }}" >{{ env('CONTACT_EMAIL') }}</a>.
+Your <b class="bold">{{ $spot->name }} Spot</b> has been removed by a moderator.
 @endif
+
+<br />
+If you believe this was an error, please contact us at <a href="mailto:{{ env('CONTACT_EMAIL') }}" >{{ env('CONTACT_EMAIL') }}</a>.
 
 Thanks,<br />
 {{ env('ORG_NAME') }}
